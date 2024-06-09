50f235b0f4a51260d62b26b7984b99abe1b55c6b
NexusSfan <nexussfan@duck.com>
2024-06-09 20:25:11 +0000

    Update convert_noip.py

--- scripts/convert_noip.py
+++ scripts/convert_noip.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 hosts = open("hosts.txt")
+hosts_file = hosts.read()
 newhosts = open("hosts_noip.txt", "xt")
-newhosts.write(hosts.replace("0.0.0.0 ", ""))
+newhosts.write(hosts_file.replace("0.0.0.0 ", ""))
 newhosts.close()
-hosts.close
+hosts.close()
